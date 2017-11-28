d887e84e31f5b2f609b29dbefd49eed12682c689
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2017-11-29 00:13:51 +0100

    improved WINDOWS script

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @IF [%1] EQU [] (SET YEAR=2013) else (SET YEAR=%1)
 @IF [%2] EQU [] (SET BITS=x64)  else (SET BITS=%2)
 
+@echo.
+@powershell -command write-host -foreground "red" -background "yellow" -nonewline "Select Compiler Visual Studio %YEAR% "
+@echo.
+
 @IF %YEAR% == 2010 (
   @set STR="Visual Studio 10 2010"
 ) ELSE IF %YEAR% == 2012 (
@@ -13,7 +17,7 @@
   @set STR=Visual Studio 15 2017
 ) ELSE (
   @echo.
-  powershell -command write-host -foreground "red" -background "yellow" -nonewline "Unsupported %YEAR%"
+  @powershell -command write-host -foreground "red" -background "yellow" -nonewline "Unsupported %YEAR%"
   @echo.
   GOTO:eof
 )
@@ -28,19 +32,39 @@
 @IF "%BITS%" == "x64" (@set STR=%STR% Win64)
 
 
-@IF NOT EXIST lib\Debug\embedFiglet.lib (
+@SET COMPILE="YES"
+@IF EXIST lib\Debug\embedFiglet.lib (
+  @IF EXIST lib\Release\embedFiglet.lib (
+    @IF EXIST lib\include\Figlet.hh (
+      @SET COMPILE="NO"
+	)
+  )
+)
 
-  @SET VSDIR=vs%YEAR%_%BITS%
+@echo.
+@powershell -command write-host -foreground "red" -background "yellow" -nonewline "YEAR=%YEAR%, BITS=%BITS%, LAPACK=%LPK%, COMPILE=%COMPILE%"
+@echo.
 
+@SET VSDIR=vs%YEAR%_%BITS%
+
+@IF %COMPILE% == "YES" (
   @RMDIR /S /Q %VSDIR%
   @mkdir %VSDIR%
   @cd %VSDIR%
+  @echo.
+  @powershell -command write-host -foreground "red" -background "yellow" -nonewline "cmake -G \"%STR%\" -D%LAPACK%=1 -DYEAR=%YEAR% -DBITS=%BITS% -DCMAKE_INSTALL_PREFIX:PATH=..\lib .."
+  @echo.
   @cmake -G "%STR%" -D%LAPACK%=1 -DYEAR=%YEAR% -DBITS=%BITS% -DCMAKE_INSTALL_PREFIX:PATH=..\lib ..
   @cmake --build . --config Release --target Install
   @cmake --build . --config Debug --target Install
   @cd ..
 ) else (
   @echo.
-  powershell -command write-host -foreground "red" -background "yellow" -nonewline "Clothoids already compiled"
+  powershell -command write-host -foreground "red" -background "yellow" -nonewline "Figlet already compiled"
   @echo.
 )
+
+@echo.
+@powershell -command write-host -foreground "red" -background "yellow" -nonewline "Figlet all done!"
+@echo.
+
