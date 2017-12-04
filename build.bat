1521754506683c3b866c605fc9bd706741666ede
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2017-12-04 20:27:59 +0100

    updated windows compilation scritpt

--- build.bat
+++ build.bat
@@ -56,6 +56,7 @@
   @echo.
   @cmake -G "%STR%" -D%LAPACK%=1 -DYEAR=%YEAR% -DBITS=%BITS% -DCMAKE_INSTALL_PREFIX:PATH=..\lib ..
   @cmake --build . --config Release --target Install
+  @cmake -G "%STR%" -D%LAPACK%=1 -DYEAR=%YEAR% -DBITS=%BITS% -DCMAKE_INSTALL_PREFIX:PATH=..\lib_debug ..
   @cmake --build . --config Debug --target Install
   @cd ..
 ) else (
