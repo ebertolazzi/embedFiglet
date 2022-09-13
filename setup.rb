16d8683f3c943fe7c6ba23c4a3ef74dd127ac361
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2022-09-13 10:58:29 +0200

    added setup.rb

---
+++ setup.rb
@@ -0,0 +1,11 @@
+puts "Setup submodules"
+system('git submodule foreach --recursive git submodule init')
+system('git submodule foreach --recursive git submodule update')
+system('git submodule foreach --recursive git submodule sync')
+
+puts ARGV
+
+if ARGV.size() > 0 && ARGV[0] == "--last" then
+  puts "\nUpdate submodules to last version"
+  system('git submodule foreach --recursive git pull')
+end
